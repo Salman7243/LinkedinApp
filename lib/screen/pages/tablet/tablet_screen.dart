4fcd66ab14729ff53145ef69adcccc6d07debf6e
Salman7243 <98895288+Salman7243@users.noreply.github.com>
2023-02-12 18:10:37 +0400

    Add files via upload
    today

---
+++ lib/screen/pages/tablet/tablet_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:linkedin_app/screen/pages/tablet/nav_bar_tablet.dart';
+import 'package:linkedin_app/screen/pages/web/body_content.dart';
+
+
+class TabletScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            NavBarTablet(),
+            Expanded(child: BodyContent()),
+          ],
+        ),
+      ),
+    );
+  }
+}
